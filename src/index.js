3efea0c103ae679cf96a138d6f360f3053889ed3
newpowalex <newpowalex@gmail.com>
2023-08-12 01:47:06 -0400

    Updated code to allow bot proper intents

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-const { Client, IntentsBitField } = require('discord.js');
 require('dotenv').config();
+const { Client, IntentsBitField} = require('discord.js');
 
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds, 
-        IntentsBitField.Flags.GuildMessages
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
     ],
 });
 
-client.once('ready', () => {
+client.once('ready', (c) => {
     console.log(`✔️  Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
+
     // Ignore messages from bots and non-text channels
+    console.log('message: ' + message.content)
     if (message.author.bot || !message.content.startsWith('!')) {
         return;
     }
@@ -21,12 +25,10 @@ client.on('messageCreate', (message) => {
     // Parse command and arguments
     const args = message.content.slice(1).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-    console.log(command)
 
     // Handle commands
     if (command == 'start') {
         // TODO: Implement Squad Builder Showdown challenge initiation
-        console.log('Started');
     } else if (command == 'guess') {
         // TODO: Implement player guesses
     } else if (command == 'reveal') {
