98f2ae4c02a853f6ed32cfc3cf80810cb149a1d8
newpowalex <newpowalex@gmail.com>
2023-08-13 22:03:13 -0400

    First commit for alt-method

--- src/index.js
+++ src/index.js
@@ -1,121 +1,55 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
+const client = new Client();
 
-const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.DirectMessages,
-        IntentsBitField.Flags.MessageContent
-    ],
-});
+const players = {};
 
-client.once('ready', () => {
-    console.log(`✔️  Logged in as ${client.user.tag}`);
-});
-
-let gameStarted = false;
-let players = {
-    player1: null,
-    player2: null
-};
-
-let currentRound = 'formation';
-
-let guesses = {
-    player1: {
-        formation: [],
-        attackers: [],
-        midfielders: [],
-        outsidebacks: [], 
-        centrebacksgk: []
-    },
-    player2: {
-        formation: [],
-        attackers: [],
-        midfielders: [],
-        outsidebacks: [],
-        centrebacksgk: []
-    }
-};
-
-async function moveToNextRound(round, players) {
-    currentRound = round;
-}
-
-async function handleGuess(player, content, currentRound, players) {
-    const guessKey = currentRound.toLowerCase();
-
-    if (content.toLowerCase() === 'done') {
-        if (!guesses[player.tag][guessKey]) {
-            await player.send(`You haven't provided any guesses for ${currentRound} yet. Please provide your guess or type "done".`);
+client.on('messageCreate', async (message) => {
+    if (message.content === '!start') {
+        // Check if the game is already in progress
+        if (players.player1 || players.player2) {
+            await message.reply('A game is already in progress.');
             return;
         }
 
-        await player.send(`Guesses for ${currentRound} saved. You can now reveal your guesses with "!reveal ${player.tag}"`);
+        // Set the first player as Player 1
+        players.player1 = message.author;
 
-        // Determine the next round
-        const nextRound = nextRoundType(currentRound);
-        if (nextRound) {
-            moveToNextRound(nextRound, players);
-            await players.player1.send(`Welcome to round ${currentRound}! Please provide your guess.`);
-            await players.player2.send(`Welcome to round ${currentRound}! Please provide your guess.`);
-        }
-    } else {
-        // Save the guess for the current round
-        if (!guesses[player.tag][guessKey]) {
-            guesses[player.tag][guessKey] = [];
-        }
-        guesses[player.tag][guessKey].push(content);
-        await player.send(`Guess for ${currentRound} saved. Please provide your next guess or type "done" to finish.`);
+        // Send an embedded message in the chat
+        const embed = new MessageEmbed()
+            .setTitle('Squad Builder Showdown')
+            .setDescription('Welcome to the game! React with ✅ to start.')
+            .setColor('#0099ff');
+
+        const sentMessage = await message.channel.send({ embeds: [embed] });
+        sentMessage.react('✅');
     }
-}
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.emoji.name === '✅' && user.bot === false && players.player1 === user) {
+        // Handle the game logic here based on the reaction
+        // Update the embedded message as needed
+        // Implement the game flow using reactions
+
+        // After handling, send DM to Player 2
+        if (!players.player2) {
+            players.player2 = user;
+            await user.send('You are Player 2! Please provide your guess for the current round through DM.');
+        }
+    }
+});
 
 client.on('messageCreate', async (message) => {
-    if (message.author.bot) {
-        return;
-    }
+    if (message.channel.type === 'DM' && (message.author === players.player1 || message.author === players.player2)) {
+        // Handle guesses and game logic for DM messages here
+        // Use the message.content from DMs to process guesses
 
-    // Handle message for bot read
-    const args = message.content.slice(1).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+        // You can also send a reply to the player through DMs
+        // await message.author.send('Your guess has been received.');
 
-    if (command === 'sbs') {
-        if (gameStarted) {
-            await message.reply('A game is already in progress.');
-        } else {
-            gameStarted = true;
-            players.player1 = message.author; // Set the player who used the command as Player 1
-            await message.reply('You are Player 1! Mention Player 2 with @');
-        }
-    } else if (gameStarted && !players.player2 && message.mentions.users.size === 1) {
-        const mentionedUser = message.mentions.users.first();
-
-        // Check if the mentioned user is not Player 1
-        if (mentionedUser.id == players.player1.id) {
-            players.player2 = mentionedUser; // Set the mentioned user as Player 2
-            await message.reply(`Both players selected: ${players.player1.tag} and ${players.player2.tag}.`);
-            await players.player1.send('Welcome to the Squad Builder Showdown game! Please provide your formation guess.');
-            await players.player2.send('Welcome to the Squad Builder Showdown game! Please provide your formation guess.');
-        }
-    } else if (gameStarted && players.player1 !== null && players.player2 !== null && message.author.id === players.player1.id) {
-        console.log('Handling');
-        handleGuess(players.player1, message.content, currentRound, message);
-    } else if (gameStarted && !players.player1 == null && !players.player2 == null && message.author.id === players.player2.id) {
-        handleGuess(players.player2, message.content, currentRound, message);
-    }
-
-    console.log(guesses.player1.formation, guesses.player2.formation)
-    if (guesses.player1.formation.length !== 0 && guesses.player2.formation.length !== 0) {
-        console.log('trying to move');
-        moveToNextRound('midfielders', players, message);
-    } else if (guesses.player1.midfielders && guesses.player2.midfielders) {
-        moveToNextRound('outsideBacks', players, message);
-    } else if (guesses.player1.outsideBacks && guesses.player2.outsideBacks) {
-        moveToNextRound('centreBacksGk', players, message);
-    } else if (guesses.player1.centreBacksGk && guesses.player2.centreBacksGk) {
-        // All rounds are completed, you can perform final actions here
+        // Example: Log the guesses for demonstration
+        console.log(`${message.author.tag} guessed: ${message.content}`);
     }
 });
 
