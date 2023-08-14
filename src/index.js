4b298e7729888325091bf3a22f07509dc3feb1a9
newpowalex <newpowalex@gmail.com>
2023-08-14 03:05:28 -0400

    Added start round function and lock in feature

--- src/index.js
+++ src/index.js
@@ -11,18 +11,54 @@ const client = new Client({
     ]
 });
 
-const players = {};
+let players = {
+    player1: {
+        locked: undefined,
+        revealed: undefined,
+        formation: [],
+        attackers: [],
+        midfielders: [],
+        outsidebacks: [],
+        centrebacksgk: []
+    },
+    player2: {
+        locked: undefined,
+        revealed: undefined,
+        formation: [],
+        attackers: [],
+        midfielders: [],
+        outsidebacks: [],
+        centrebacksgk: []
+    }
+};
+let currentRound = 'formation';
+
+async function startRound(players) {
+    // Send DMs to both players with welcome message and current round info
+    for (const player of Object.values(players)) {
+        const embed = new EmbedBuilder()
+            .setTitle('Squad Builder Showdown')
+            .setDescription(`Welcome to round ${currentRound}, ${player.tag}! Please provide your guess.`)
+            .setColor('#0099ff');
+
+        const dmChannel = await player.createDM();
+        await dmChannel.send({ embeds: [embed] });
+    }
+}
 
 client.on('messageCreate', async (message) => {
     if (message.content === '!sbs') {
         // Check if the game is already in progress
-        if (players.player1 || players.player2) {
+        if (players.player1.tag || players.player2.tag) {
             await message.reply('A game is already in progress.');
             return;
         }
 
         // Set the first player as Player 1
         players.player1 = message.author;
+        console.log(players.player1.tag)
+        console.log(players.player2.tag)
+
 
         // Send an embedded message in the chat
         const embed = new EmbedBuilder()
@@ -36,9 +72,10 @@ client.on('messageCreate', async (message) => {
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
-    if (reaction.emoji.name === '✅' && user.bot === false && players.player1 !== user && !players.player2) {
+    if (reaction.emoji.name === '✅' && user.bot === false && players.player1 !== user && !players.player2.tag) {
         // Set the first person who reacts as Player 2
         players.player2 = user;
+        console.log(players.player2.tag)
 
         // Remove reactions from the message
         reaction.message.reactions.removeAll();
@@ -50,13 +87,40 @@ client.on('messageReactionAdd', async (reaction, user) => {
             .setColor('#0099ff');
 
         reaction.message.edit({ embeds: [embed] });
+        
+
+        // Start the first round by sending DMs to players
+        startRound(players);
+        await reaction.message.react('🔒');
+    }
+
+    if (reaction.emoji.name === '🔒' && user.bot === false && (players.player1 === user || players.player2 === user)) {
+        console.log(`Someone locked: ${user.tag}  | Locked?: ${user.locked}`)
+        if (typeof user.locked === 'undefined') {
+            user.locked = true;
+            console.log(user.tag, 'locked')
+        }
+
+        if (players.player1.locked === true && players.player2.locked === true) {
+            // Remove reactions from the message
+            reaction.message.reactions.removeAll();
+
+            const embed1 = new EmbedBuilder()
+                .setTitle('Squad Builder Showdown')
+                .setDescription(`Both players locked in!\n\n React with 1️⃣ to reveal ${players.player1.tag}\n\n React with 2️⃣ to reveal ${players.player2.tag}`)
+                .setColor('#0099ff');
+
+            reaction.message.edit({ embeds: [embed1] });
+            await reaction.message.react('1️⃣');
+            await reaction.message.react('2️⃣');
+        }
     }
 });
 
 client.on('messageCreate', async (message) => {
     if (message.channel.type === 'DM' && (message.author === players.player1 || message.author === players.player2)) {
         // Handle guesses and game logic for DM messages here
-        // Use the message.content from DMs to process guesses
+        const content = message.content;
 
         // You can also send a reply to the player through DMs
         // await message.author.send('Your guess has been received.');
