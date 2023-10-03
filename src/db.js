eb6c85a090ee464b71133310c6b19ac33fa0ac4c
newpowalex <newpowalex@gmail.com>
2023-10-03 17:29:25 -0400

    Created db.js for database handling

--- src/db.js
+++ src/db.js
@@ -1,22 +1,42 @@
 // db.js
 const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
 
 let db;
 
 async function init() {
-    db = await open({
-        filename: './sbsbot.db',
-        driver: sqlite3.Database,
+    // Check if there's an existing database connection
+    if (db) {
+        console.log('Closing the existing database connection.');
+        await close(); // Close the existing connection
+    }
+
+    db = new sqlite3.Database('./sbsbot.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log('Connected to the database.');
+        }
     });
 
     // Define your database schema and tables here if needed
 }
 
 async function close() {
-    if (db) {
-        await db.close();
-    }
+    return new Promise((resolve, reject) => {
+        if (db) {
+            db.close((err) => {
+                if (err) {
+                    console.error(err.message);
+                    reject(err);
+                } else {
+                    console.log('Closed the database connection.');
+                    resolve();
+                }
+            });
+        } else {
+            resolve(); // Resolve immediately if there's no open connection
+        }
+    });
 }
 
 // Export the database-related functions
